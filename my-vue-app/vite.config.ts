62db5bc6ceb220a7898b6802eaebff3cadc29e97
Shamitsu212 <darklin314@gmail.com>
2026-06-10 17:32:20 +0300

    fix: Исправлена опечатка в vite.config.ts

--- my-vue-app/vite.config.ts
+++ my-vue-app/vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/Vue_currency_converter",
+  base: "/Vue_currency_converter/",
 })
